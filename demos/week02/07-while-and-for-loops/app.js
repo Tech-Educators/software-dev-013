2163c676695ecc347da1da654ff11a09c0ffb007
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-08-21 10:57:45 +0100

    for loops

--- demos/week02/07-while-and-for-loops/app.js
+++ demos/week02/07-while-and-for-loops/app.js
@@ -1,28 +1,61 @@
 
-if (5 < 4) {
-    console.log('Hello world!')
-}
+// if (5 < 4) {
+//     console.log('Hello world!')
+// }
 
-let counter = 0
+// let counter = 0
 
-while (counter < 10) {
-    console.log('Hello world!', counter)
-    counter++
-}
+// while (counter < 10) {
+//     console.log('Hello world!', counter)
+//     counter++
+// }
 
-console.log('This is after the while loop')
+// console.log('This is after the while loop')
 
 
 // some random number
-let randomNumber = (Math.floor(Math.random() * 10) + 1)// i rolled a dice so always random
-let userAns = prompt('Guess a number between 1 - 10')
+// let randomNumber = (Math.floor(Math.random() * 10) + 1)// i rolled a dice so always random
+// let userAns = prompt('Guess a number between 1 - 10')
 
-while (randomNumber != userAns) {
-    alert('You got that wrong')
-    userAns = prompt('Guess a number between 1 - 10')
-}
+// while (randomNumber != userAns) {
+//     alert('You got that wrong')
+//     userAns = prompt('Guess a number between 1 - 10')
+// }
 
-alert('You got everything right!!')
+// alert('You got everything right!!')
 
 // When do we use while loops?
 // When we don't know how often we need to loop. 
+
+
+
+/*
+
+------------------ FOR LOOPS ------------------------
+
+*/
+
+// for (counter; a condition; somethingtoUpdateTheConditon) {
+//     // whatever I want to do.
+// }
+// 1) declare a variable in our parenthesis followed by ;'s
+// 2) set up a conditon, followed by ;'s
+for (let counter = 0; counter < 5; counter++) {
+    // logs the value of the 'counter' varible
+    console.log(`the value of ${counter}`)
+}
+
+// we only intialize the variable counter once at the beginning.
+
+/// > Check condition
+/// > run the code in the {}\s
+/// > update varible/afterthought.
+
+
+// run whatever else to do. 
+
+// the value of counter 0
+// the value of counter 1
+// the value of counter 2
+// the value of counter 3
+// the value of counter 4
