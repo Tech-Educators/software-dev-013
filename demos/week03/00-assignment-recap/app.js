3049bfaaaa08a56ee71c4fcc82d40053a3120081
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-08-27 10:09:30 +0100

    loops exercise

---
+++ demos/week03/00-assignment-recap/app.js
@@ -0,0 +1,38 @@
+// create a div for menu, and img element for the background - rest done in javascript. 
+
+// we'll have an array of image objects 
+// looping through arrays 
+
+
+let myArray = [{number: 1, altText: 'THis is the number1'},
+    {number: 2},
+    {number: 3},
+    {number: 4}]
+
+myArray.forEach(function(number) {
+    console.log(number * 2)
+})
+
+
+function foo() {
+    
+}
+// 1
+// 2
+// 3
+// 4
+
+
+for (let i = 0; i < myArray.length; i++) {
+    // console.log(myArray[i])
+    // console.log((myArray[i].number * 2), myArray[i].altText)
+}
+// HTML image element vs DATA about an image 
+
+
+let myInfo = {
+    name: 'Sam',
+    city: 'Liverpool'
+}
+
+console.log(myInfo.city) //Liverpool
---
+++ demos/week03/00-assignment-recap/exercise.js
@@ -0,0 +1,21 @@
+// using this array of pet objects, log each of the pets information to the console one by one in a string 
+// use either forEach, forOf, or a 'traditional' for loop.
+// eg - `Buddy is a Dog, who is 3 years old` 
+
+const pets = [
+    {
+      name: "Buddy",
+      type: "Dog",
+      age: 3
+    },
+    {
+      name: "Whiskers",
+      type: "Cat",
+      age: 2
+    },
+    {
+      name: "Nibbles",
+      type: "Hamster",
+      age: 1
+    }
+  ];
