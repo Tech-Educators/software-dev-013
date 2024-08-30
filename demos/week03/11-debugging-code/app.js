d8132c0b6de7f5d77322309e306f4845b66be441
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-08-30 09:25:57 +0100

    debugger

---
+++ demos/week03/11-debugging-code/app.js
@@ -0,0 +1,15 @@
+const samsFavNumber = 42;
+debugger;
+
+
+for (let i = 0; i < 100; i++) {
+    let x = 10 + i
+}
+
+function add(a, b) {
+    let sum = a + b
+    console.log(sum)
+    debugger;
+}
+
+add(5, 5)
