e62bba32d6ca995430ea08f990cf0b3dc93a8a3c
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-09-17 08:38:37 +0100

    react

--- demos/week06/04-react-props/src/components/Gallery.jsx
+++ demos/week06/04-react-props/src/components/Gallery.jsx
@@ -4,7 +4,7 @@ export default function Gallery() {
     return (
         <div>
             <Painting img_url='https://upload.wikimedia.org/wikipedia/commons/3/33/The_Roses_of_Heliogabalus.jpg'/>
-            <Painting img_url='https://miro.medium.com/v2/resize:fit:584/1*58vziXRx1l8pQBSQVeHABg.jpeg' toCool={true}/>
+            <Painting img_url='https://miro.medium.com/v2/resize:fit:584/1*58vziXRx1l8pQBSQVeHABg.jpeg' tooCool={true}/>
             <Painting img_url='https://cdn.britannica.com/45/1245-004-676A6721/Kanagawa-Wave-woodblock-colour-print-series-part.jpg'/>
             <Painting img_url='https://substackcdn.com/image/fetch/w_1456,c_limit,f_webp,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F5b87fa9a-112a-4c4d-a7c9-d368266319d4_1426x1445.webp'/>
         </div>
