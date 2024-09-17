84261701d85884943d6b359be1d831e57556a4aa
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-09-17 15:32:55 +0100

    conditoinal rendering

---
+++ demos/week06/08-conditional-rendering/src/App.jsx
@@ -0,0 +1,30 @@
+import {useState} from "react"
+
+export default function App() {
+
+  const [showHippo, setShowHippo] = useState(false)
+
+  function toggleHippo() {
+    setShowHippo(!showHippo)
+  }
+
+  return (
+    <div>
+
+      <button onClick={toggleHippo}>Show hippo</button>
+
+        {/* ternary opator  */}
+      {
+            showHippo ? 
+            <img src='https://www.balisafarimarinepark.com/wp-content/uploads/2023/05/zyro-image-1024x637.jpg' alt='baby hippo' /> 
+            : 
+            <img src='https://www.birdlife.org/wp-content/uploads/2021/06/Hummingbird-Norbert-Hentges-Unsplash-edited-scaled.jpg' alt='humming bird' />
+      }
+        {/* just another way to hiding the elemtn */}
+        {/* using && to short-curcuit */}
+      {showHippo && <img src='https://www.balisafarimarinepark.com/wp-content/uploads/2023/05/zyro-image-1024x637.jpg' alt='baby hippo' /> }
+
+      {/* <img src='https://www.balisafarimarinepark.com/wp-content/uploads/2023/05/zyro-image-1024x637.jpg' alt='baby hippo' /> */}
+    </div>
+  )
+}
