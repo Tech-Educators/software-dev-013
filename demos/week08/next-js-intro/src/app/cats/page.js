b29d7d3809e3a3b6aae3ebea0ef1c532aaa859c6
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-09-30 13:29:27 +0100

    nextjs

---
+++ demos/week08/next-js-intro/src/app/cats/page.js
@@ -0,0 +1,7 @@
+export default function CatsPage() {
+    return (
+        <div>
+            <p>This page is about cats! Nextjs uses directory names to create routes for your application! A page.js is used to create the page that shows up on that route. It is important that it&apos;s lowercase</p>
+        </div>
+    )
+}
--- demos/week08/next-js-intro/src/app/page.js
+++ demos/week08/next-js-intro/src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <p>This is the home route</p>
+      <h1>This is my new framework!! How cool! FRIENDSHIP ENDED WITH REACT, NOW NEXTJS IS MY BEST FRIEND.</h1>
     </div>
   );
 }
